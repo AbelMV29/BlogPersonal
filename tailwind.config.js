843886cd1c0df03f47e2b7463b71e2ad2c7731b7
abelmv <mvabel23@gmail.com>
2024-08-03 12:45:21 -0300

    Agregar archivos de proyecto.

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './Pages/**/*.cshtml',
+        './Views/**/*.cshtml'
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
